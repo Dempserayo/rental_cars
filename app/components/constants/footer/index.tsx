f216ac2f472cb728f051f781b076f7c268c5a882
Raye <160432415+Dempserayo@users.noreply.github.com>
2026-04-01 15:08:54 -0500

    .

---
+++ app/components/constants/footer/index.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="flex justify-between items-center p-4">
+      <h1 className="text-2xl font-bold">Outlet Rental Car</h1>
+    </footer>
+  );
+}
---
+++ app/components/constants/navbar/index.tsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav className="w-full max-w-7xl h-24 bg-black flex flex-row items-center justify-between p-10">
+        <section className="w-auto font-thin">
+            <h1>Rental</h1>
+        </section>
+        <section className="w-auto bg-red-500">
+            <h1>Rental</h1>
+        </section>
+    </nav>
+  );
+}
